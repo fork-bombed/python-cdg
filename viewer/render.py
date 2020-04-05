70f271a49048d35f2e15410d41f78b9a18d7c007
fork-bombed <ryanjmurphy2016@gmail.com>
2020-04-05 01:24:20 +0100

    Cleaned up render view

--- viewer/render.py
+++ viewer/render.py
@@ -1,22 +1,15 @@
 import cv2
 import numpy as np
-from bitstring import BitArray
 from color import Color
-from packet import *
+import packet
 
 WINDOW_NAME = 'CD+G'
 
 class Window:
     def __init__(self,name):
         self.name = name
-        # CD-G is 288 pixels across and 192 pixels high
-        img = np.ones((192,288,3), np.uint8)
-        self.previous = img
-        cv2.namedWindow(WINDOW_NAME,flags=cv2.WINDOW_GUI_NORMAL)
-        # Create nicely sized screen with 3:2 aspect ratio
-        cv2.resizeWindow(WINDOW_NAME,900,600)
-        # Create empty window
-        cv2.imshow(WINDOW_NAME, img)
+        self.previous = None
+        self._create_window()
     def resize(self,width,height):
         cv2.resizeWindow(WINDOW_NAME,width,height)
     def wait(self):
@@ -24,19 +17,24 @@ class Window:
     def update(self,img):
         cv2.imshow(WINDOW_NAME, img)
         self.previous=img
+    def _create_window(self):
+        # CD-G is 288 pixels across and 192 pixels high
+        img = np.ones((192,288,3), np.uint8)
+        self.previous = img
+        cv2.namedWindow(WINDOW_NAME,flags=cv2.WINDOW_GUI_NORMAL)
+        cv2.resizeWindow(WINDOW_NAME,900,600)
+        cv2.imshow(WINDOW_NAME, img)
     def render(self,bytestream):
-        # every 24 bytes, print
+        # every 24 bytes
         jump=24
         pos=0
         while pos<=len(bytestream)-jump:
             block=bytestream[pos:pos+jump]
-            if block[0]==9:
-                packet = Packet(block)
-                # if cmd and (cmd==table[30] or cmd==table[31]):
-                if packet.instruction == COLOR_TABLE_LOWER or packet.instruction == COLOR_TABLE_UPPER:
-                    print(packet)
-                    # data = block.hex()
-                    # print(' '.join([data[i:i+2].upper() for i in range(0, len(data), 2)]))
+            if block[0]==packet.PACKET_BEGIN:
+                p = packet.Packet(block)
+                data = p.decode()
+                if data:
+                    print(p.name, data)
             pos+=jump
 
 
@@ -51,12 +49,7 @@ img = np.ones((192,288,3), np.uint8)
 img[10][10] = (255,255,255)
 window.update(img)
 lines = get('tester.cdg')
-window.render(lines[:100000])
-# pos=0
-# for y in range(192): # height
-#     for x in range(288): # width
-#         img[y][x]=(lines[pos],lines[pos],lines[pos])
-#         pos+=1
+window.render(lines[:10000])
 
 window.update(img)
         
